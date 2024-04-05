3fd723b40828f84434039eb4f525bfab22de2beb
lakshjain1 <lakshjain0498@gmail.com>
2024-04-06 00:03:16 +0530

    Update

--- 01-Hello.sh
+++ 01-Hello.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello World, I'm learning DevOps with AWS"
+PERSON1=Laksh
+PERSON2=Aditi
+
+echo "$PERSON1: Hi $PERSON2, Good morning"
+echo "$PERSON2: Good Morning..!! $PERSON1"
