b41fc6a0cb7ed49564b9a314fa4ca97769d56467
lakshjain1 <lakshjain0498@gmail.com>
2024-04-06 00:03:54 +0530

    Update

--- 01-Hello.sh
+++ 01-Hello.sh
@@ -6,3 +6,5 @@ PERSON2=Aditi
 
 echo "$PERSON1: Hi $PERSON2, Good morning"
 echo "$PERSON2: Good Morning..!! $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
