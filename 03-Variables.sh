fe0afcb3ecd193206d2a0d488f3d71deada85a5e
lakshjain1 <lakshjain0498@gmail.com>
2024-04-06 00:12:03 +0530

    Update

---
+++ 03-Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello World, I'm learning DevOps with AWS"
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good morning"
+echo "$PERSON2: Good Morning..!! $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
