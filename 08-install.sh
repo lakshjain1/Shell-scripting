760fb586616213a0569250290d23da39160ff534
lakshjain1 <lakshjain0498@gmail.com>
2024-04-06 17:09:55 +0530

    latest 1

--- 08-install.sh
+++ 08-install.sh
@@ -2,9 +2,12 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] #give space in [ in this ]
 then
     echo "ERROR:Please run this script with root user"
+    exit 1
 else
     echo " you are root user"
 fi
+
+yum install git -y
