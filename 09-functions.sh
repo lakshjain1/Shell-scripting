8582ce885b870ae3a070a1d9c3074dbc28a8430d
lakshjain1 <lakshjain0498@gmail.com>
2024-04-08 16:18:46 +0530

    New Latest

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-USERID=$(id -u)
+DATE=($date +%F-%H-%M-%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
 VALIDATE(){
     if [ $? -ne 0 ]
     then
-        echo "Error installing git"
+        echo "Error installing $2"
         exit 1
     else
-        echo "Installed success"
+        echo "Installed success $2"
     fi
 }
+
+USERID=$(id -u)
+
 
 if [ $USERID -ne 0 ] #give space in [ in this ]
 then
