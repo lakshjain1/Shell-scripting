ed770ab38edc8903e113ef530f8c81b8227fe306
lakshjain1 <lakshjain0498@gmail.com>
2024-04-08 16:42:11 +0530

    New Latest

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,13 +4,17 @@ DATE=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "Error installing $2"
+        echo -e "$R Error installing $2 $N"
         exit 1
     else
-        echo "Installed success $2"
+        echo -e "$G Installed success $2 $N"
     fi
 }
 
